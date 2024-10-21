3bbd94725efe8bc1b669cc1384379b4799bd8d2d
Sydney Wathuta <swathuta@uvm.edu>
2024-10-21 08:28:43 -0600

    first commit

---
+++ Mad libs/index.js
@@ -0,0 +1,15 @@
+const typeOfBirdInput = document.getElementById('typeBird');
+const typeBird = typeOfBirdInput.Value;
+
+const RoommateName = document.getElementById('Roommate');
+const Roommate = RoommateName.Value;
+
+const Noun = document.getElementById('noun');
+const noun = Noun.Value;
+
+const Liquid = document.getElementById('liquid');
+const liquid = Liquid.Value;
+
+const BodyParts = document.getElementById('bodyParts');
+const bodyParts = BodyParts.Value;
+
